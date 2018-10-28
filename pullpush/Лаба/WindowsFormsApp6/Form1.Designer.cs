ab4e5b4b399c2232c4188e2b11dbd225a7851fda
Vladislav Mischenko <ioopudoff@yandex.ru>
2018-10-28 23:30:14 +0700

    Add new document

---
+++ pullpush/Лаба/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.data = new System.Windows.Forms.DataGridView();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // data
+            // 
+            this.data.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data.Dock = System.Windows.Forms.DockStyle.Top;
+            this.data.Location = new System.Drawing.Point(0, 0);
+            this.data.Name = "data";
+            this.data.RowHeadersVisible = false;
+            this.data.Size = new System.Drawing.Size(822, 396);
+            this.data.TabIndex = 0;
+            this.data.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.data_CellClick);
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.Color.White;
+            this.Delete.Location = new System.Drawing.Point(712, 406);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(98, 30);
+            this.Delete.TabIndex = 1;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Update
+            // 
+            this.Update.BackColor = System.Drawing.Color.White;
+            this.Update.Location = new System.Drawing.Point(127, 404);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(103, 30);
+            this.Update.TabIndex = 2;
+            this.Update.Text = "Изменить";
+            this.Update.UseVisualStyleBackColor = false;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.Color.White;
+            this.Add.Location = new System.Drawing.Point(12, 404);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(92, 30);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = false;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(822, 441);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.data);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView data;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Add;
+    }
+}
+
